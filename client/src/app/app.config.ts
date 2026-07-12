7f7cde05335367215f84a37c5a94eac6562dadc5
jack <jack>
2026-07-11 21:15:59 -0400

    refactor(ui): remove legacy component libraries

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,7 @@ import {InitialModule} from "./initial.module";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import {provideRouter, withComponentInputBinding} from "@angular/router";
 import {routes} from "./app.routes";
-import {DatePipe} from "@angular/common";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {providePrimeNG} from "primeng/config";
-import Aura from '@primeng/themes/aura';
-import {CalendarModule, DateAdapter} from "angular-calendar";
-import {adapterFactory} from "angular-calendar/date-adapters/date-fns";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,16 +13,7 @@ export const appConfig: ApplicationConfig = {
       BrowserModule,
       BrowserAnimationsModule,
       InitialModule,
-      CalendarModule.forRoot({
-        provide: DateAdapter,
-        useFactory: adapterFactory,
-      }),
     ),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
   ]
 };
--- client/src/app/initial.module.ts
+++ client/src/app/initial.module.ts
@@ -7,12 +7,8 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule} from "@angular/forms";
 import {NgxRequestModule} from "@kwyxyz/ngx-request";
-import {NgxAuthModule} from "@kwyxyz/ngx-auth";
 import {environment} from "../environments/environment";
 import {NotificationService} from "@project-shared/services/notification.service";
-import {ConfirmationService, LoadingService, NgxCommonModule} from "@kwyxyz/ngx-common";
-import {RoutingPart} from "./_utils/const";
-import {CustomFormlyModule} from './custom-formly.module';
 
 
 @NgModule({
@@ -22,9 +18,7 @@ import {CustomFormlyModule} from './custom-formly.module';
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    NgxCommonModule,
     NgxRequestModule,
-    NgxAuthModule,
   ],
   imports: [CommonModule,
     BrowserModule,
@@ -36,33 +30,15 @@ import {CustomFormlyModule} from './custom-formly.module';
         deps: [HttpClient],
       }
     }),
-    NgxCommonModule.forRoot({
-      allowMetaInPage: true
-    }),
     NgxRequestModule.forRoot({
       defaultApiUrl: environment.baseUrl,
-      confirmationService: ConfirmationService,
       notificationService: NotificationService,
-      loadingService: LoadingService,
       defaultWsUrl: environment.wsBaseUrl,
       defaultPublicWs: 'public',
       userSocketUrl: 'user-socket/me',
       dateFields: ["recorded_at", "started_at", "ended_at"]
     }),
-    NgxAuthModule.forRoot({
-      confirmationService: ConfirmationService,
-      notificationService: NotificationService,
-      allowedUrls: [environment.baseUrl],
-      authApiUrl: environment.authApiUrl,
-      allowedOauthProvider: [],
-      loginPageUri: RoutingPart.login,
-      afterLoginPageUri: `/${RoutingPart.app}`,
-      afterLogoutPageUri: `/${RoutingPart.login}`,
-      afterRegistrationPageUri: `/${RoutingPart.login}`,
-      afterPasswordResetPageUri: `/${RoutingPart.login}`
-    }),
-    ReactiveFormsModule,
-    CustomFormlyModule
+    ReactiveFormsModule
   ], providers: [
     provideHttpClient(withInterceptorsFromDi()),
 
